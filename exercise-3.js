a632e86c17325b55a4885316e6f0aa0a9aab0f20
zniode <zakaria.niode@gmail.com>
2018-08-05 21:07:55 +0700

    Mengubah file exercise-3.js

--- exercise-3.js
+++ exercise-3.js
@@ -1,17 +1,17 @@
-var nama = 'Zakaria';
-var peran = 'Ksatria';
+var nama = '';
+var peran = '';
 
-if (nama === ''  && peran === '' ) {
+if (nama === '') {
   console.log('Nama harus diisi!');
-} else if (nama === 'Zakaria' && peran === '') {
+} else if (peran === '') {
   console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!');
-} else if (nama === 'Zakaria' && peran === 'Ksatria') {
+} else if (peran === 'Ksatria') {
   console.log('Selamat datang di Dunia Proxytia, ' + nama);
   console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
-} else if (nama === 'Zakaria' && peran === 'Tabib' ) {
+} else if (peran === 'Tabib' ) {
   console.log('Selamat datang di Dunia Proxytia, ' + nama);
   console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.');
-} else if (nama === 'Zakaria' && peran === 'Penyihir') {
+} else if (peran === 'Penyihir') {
   console.log('Selamat datang di Dunia Proxytia, ' + nama);
   console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
 }   
